49a82e5d24ad399045f77df31351df56e35745e9
Ben Lubar <ben.lubar@gmail.com>
2018-05-14 11:22:36 -0500

    Reorganize into Inedo.SDK extension. Update README.md and LICENSE.

---
+++ MySql/InedoExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MySQL")]
+[assembly: AssemblyDescription("Database integration for MySQL 4.0 and later.")]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
